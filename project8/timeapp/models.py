5834379671bdc441c9d7ffd80ba5ef9fad550435
venky794 <venkeysubbu7@gmail.com>
2021-06-23 22:53:09 +0530

    addes student model class

--- project8/timeapp/models.py
+++ project8/timeapp/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here
 class Employee(models.Model):
 	name=models.CharField(max_length=100)
+
+
+class student(models.Mode):
+	name=models.CharField(max_length=200)
